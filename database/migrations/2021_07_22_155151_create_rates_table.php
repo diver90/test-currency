863b5ba39d6d4a2b63c3ff6b52add91d57499f39
diver90 <mihail.zavalniuk@gmail.com>
2021-07-22 20:31:52 +0300

    create model for rates table

---
+++ database/migrations/2021_07_22_155151_create_rates_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rates', function (Blueprint $table) {
+            $table->id();
+            $table->string('symbol');
+            $table->float('rate', '15', '8');
+            $table->integer('timestamp');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rates');
+    }
+}
